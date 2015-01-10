c86f91948e2c1f112b11ca49a9729eda4134f254
jaswsinc <jas@wsharks.com>
2015-01-10 06:59:32 -0900

    Shortcode :: Articles List
    - Initializing list template file.
    
    See: websharks/wp-kb-articles#5

--- wp-kb-articles/includes/templates/type-a/site/articles/list.php
+++ wp-kb-articles/includes/templates/type-a/site/articles/list.php
@@ -1,10 +1,14 @@
 <?php
 namespace wp_kb_articles;
+
 /**
- * @var plugin      $plugin Plugin class.
- * @var template    $template Template class.
+ * @var plugin    $plugin Plugin class.
+ * @var template  $template Template class.
  *
  * Other variables made available in this template file:
+ *
+ * @var \stdClass $attr Parsed/normalized/validated shortcode attributes.
+ * @var \WP_Query $query WP Query class instance ready for iteration.
  *
  * -------------------------------------------------------------------
  * @note In addition to plugin-specific variables & functionality,
